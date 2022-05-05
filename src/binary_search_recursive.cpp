6339651f270e5beef966bd56b85ccf5da1b4b023
Ruslan <khanin.rus@mail.ru>
2022-05-05 23:52:02 +0300

    First commit

--- src/binary_search_recursive.cpp
+++ src/binary_search_recursive.cpp
@@ -7,14 +7,26 @@
 namespace assignment {
 
   std::optional<int> BinarySearchRecursive::Search(const std::vector<int>& arr, int search_elem) const {
-    // запускаем рекурсивный метод с границами поиска от начала массива до конца
-    return search(arr, search_elem, 0, static_cast<int>(arr.size() - 1));
+    return recursive_helper(arr, search_elem, 0, static_cast<int>(arr.size() - 1));
   }
 
-  std::optional<int> BinarySearchRecursive::search(const std::vector<int>& arr, int search_elem, int start, int stop) const {
+  std::optional<int> BinarySearchRecursive::recursive_helper(const std::vector<int>& arr, int search_elem, int left_index, int right_index) const {
+    int mid_index = (left_index + right_index) / 2;
+    if (left_index > right_index){
+      return std::nullopt;
+    }
+    if (arr[mid_index] == search_elem){
+      return mid_index;
+    }
+    if (arr[mid_index] > search_elem) {
+      return recursive_helper(arr, search_elem, left_index, mid_index - 1);
+    }
+    if (arr[mid_index] < search_elem) {
+      return recursive_helper(arr, search_elem, mid_index + 1, right_index);
+    }
 
     // Tips:
-    // 1. Рассмотрите базовые случаи выхода и рекурсии:
+    // 1. Рассмотрите базовые случаи выхода из рекурсии:
     //    1) индекс левого элемента стал больше индекса правого элемента
     //    2) целевой элемент найден
     // 2. Вызовите рекурсивный метод, изменив границы поиска
--- src/linear_search_iterative.cpp
+++ src/linear_search_iterative.cpp
@@ -4,8 +4,11 @@ namespace assignment {
 
   std::optional<int> LinearSearchIterative::Search(const std::vector<int>& arr, int search_elem) const {
 
-    // Tips: итеративно пройдитесь по элементам массива
-
+    for (int i = 0; i<arr.size(); i++) {
+      if (arr[i] == search_elem) {
+        return i;
+      }
+    }
     return std::nullopt;
   }
 
